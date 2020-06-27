9e6e893f19bed49c56ff1f552bf2f1bc1ad3e6ba
Kai Chan <chankw.jh@gmail.com>
2020-06-27 13:17:37 +0100

    add cron job walks up free host

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express, { response } from 'express';
+import cron from 'node-cron';
+import fetch from 'node-fetch';
 import ResumeService from './ResumeService';
 
 const app = express();
@@ -9,12 +11,20 @@ app.get('/resume/:version', async function (request, response) {
     try {
         const resume = await resumeService.get(request.params.version);
         (resume != null) ?
-            response.status(200).json(resume) : 
+            response.status(200).json(resume) :
             response.status(404).end();
     } catch (error) {
         console.log(error);
         response.status(500).end('Something Wrong');
     }
+});
+
+//Wake up Free hosting app
+cron.schedule('*/25 * * * *', function () {
+    console.log("Running healthcheck");
+    fetch('https://plain-resume-api.azurewebsites.net/resume/release/')
+        .then( res => console.log('healthcheck success:' + res.status))
+        .catch(err => console.log('healthcheck error:' + err))
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
